c70d1183409a6a48afcccd0c095e46be39a1c77d
Arnaud Delorme <arnodelorme@gmail.com>
2025-10-27 19:04:06 +0100

    adding menus

--- PracticalMEEG_Session_1_Preprocess_Data.m
+++ PracticalMEEG_Session_1_Preprocess_Data.m
@@ -2,7 +2,8 @@
 % Wakeman & Henson Data analysis: Ereprocess Data Session #1 
 
 % Authors: Ramon Martinez-Cancino, Brain Products, 2022
-%          Arnaud Delorme, SCCN, 2022
+%          Romain Grandchamp, 2025
+%          Arnaud Delorme, SCCN, 2022-2025
 %          Johanna Wagner, Zander Labs, 2022
 %
 % Copyright (C) 2022  Johanna Wagner
@@ -32,23 +33,29 @@ filename = 'wh_S01_run_01.set';
 % Start EEGLAB
 [ALLEEG, EEG, CURRENTSET] = eeglab; 
 
-%% Loading data
+%% Loading data 
+% use menu item File > Import Data > Using File-IO
+% and import file ds000117_pruned/derivatives/meg_derivatives/sub-01/ses-meg/meg/wh_S01_run_01.set
 EEG = pop_loadset('filename', filename,'filepath',path2data);
 
 %% Re-Reference
+% use menu item Tools > Re-reference the data
 % Apply Common Average Reference
 EEG = pop_reref(EEG,[]); % menu item Tools > Rereference (eegh)
 
 %% Resampling
+% use menu item Tools > Change sampling rate
 % Downsampling to 100 Hz for speed (for real analysis prefer 250 or 500 Hz)
 EEG = pop_resample(EEG, 100);
 
 %% Filter
-% Filter the data Highpass at 1 Hz Lowpass at 90Hz (to avoid line noise at 100Hz)
+% use menu item Tools > Filter the data > Basic FIR filter
+% Filter the data Highpass at 1 Hz Lowpass at 40Hz (to avoid line noise at 100Hz)
 EEG = pop_eegfiltnew(EEG, 1, 0);   % High pass at 1Hz
 EEG = pop_eegfiltnew(EEG, 0, 40);  % Low pass below 40
 
 %% Automatic rejection of bad channels
+% use menu item Tools > Reject data using Clean_rawdata and ASR
 % Apply clean_artifacts() to reject bad channels
 if contains(EEG.chanlocs(1).type, 'meg')
     minChanCorr = 0.4;
@@ -57,8 +64,8 @@ else
 end
 EEG = clean_artifacts(EEG, 'Highpass', 'off',...
                            'ChannelCriterion', minChanCorr,...
-                           'ChannelCriterionMaxBadTime', 0.4,...
-                           'LineNoiseCriterion', 4,...
+                           'ChannelCriterionMaxBadTime', 0.4,... % not in GUI
+                           'LineNoiseCriterion', 4,... % line noise
                            'BurstCriterion', 'off',...
                            'WindowCriterion','off' );
 
@@ -70,19 +77,21 @@ EEG = clean_artifacts( EEG, 'Highpass', 'off',...
                             'ChannelCriterion', 'off',...
                             'LineNoiseCriterion', 'off',...
                             'BurstCriterion', 30,...
-                            'WindowCriterion',0.3);
+                            'WindowCriterion',0.3); % not in gui
 
 %% run ICA
-% Use second line for speed (but you must install the Picard plugin)
+% use menu item Tools > Decompose by ICA
+% Use first line for speed (but you must install the Picard plugin)
 % The -1 for the number of channel is to account for matrix rank 
 % decrease due to average reference
 if exist('picard') % faster
-    EEG = pop_runica( EEG , 'picard', 'maxiter', 500, 'pca', EEG.nbchan-1);
+    EEG = pop_runica( EEG , 'picard', 'maxiter', 500, 'pca', -1);
 else
-    EEG = pop_runica( EEG , 'runica', 'extended',1, 'pca', EEG.nbchan-1);
+    EEG = pop_runica( EEG , 'runica', 'extended',1, 'pca', -1);
 end
 
 %% automatically classify Independent Components using IC Label
+% use menu item Tools > Classify components using ICLabel > Label components
 % EEG only, MEG would be possible if ICLabel is retrained with MEG
 % components instead of EEG components
 if ~contains(EEG.chanlocs(1).type, 'meg')
@@ -90,4 +99,5 @@ if ~contains(EEG.chanlocs(1).type, 'meg')
 end
 
 %% Save dataset
+% use menu item File > Save current dataset as
 EEG = pop_saveset( EEG,'filename', 'wh_S01_run_01_preprocessing_data_session_1_out.set','filepath',path2data);
